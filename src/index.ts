3357b92f78a850193731e7bdfc34708dbd01db7e
Tony <jeehyukwon@gmail.com>
2023-01-24 19:49:18 +0900

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { pascalCase } from "change-case";
+import type { GraphQLSchema } from "graphql";
+import { isInterfaceType } from "graphql";
+import { isObjectType } from "graphql";
+
+module.exports = {
+  plugin(schema: GraphQLSchema) {
+    const lines: string[] = [];
+
+    const typeMap = schema.getTypeMap();
+
+    Object.values(typeMap).forEach((type) => {
+      if (isObjectType(type) && !type.name.startsWith("__")) {
+        lines.push(
+          `export function define${type.name}Resolvers(resolvers: Resolvers["${type.name}"]) {`,
+        );
+        lines.push(`  return resolvers;`);
+        lines.push(`}`);
+      }
+      if (isInterfaceType(type)) {
+        lines.push(
+          `export function define${type.name}Resolvers(resolvers: Resolvers["${type.name}"]) {`,
+        );
+        lines.push(`  return resolvers;`);
+        lines.push(`}`);
+      }
+    });
+
+    if (isObjectType(typeMap.Query)) {
+      Object.keys(typeMap.Query.getFields()).forEach((queryField) => {
+        lines.push(
+          `export function define${pascalCase(
+            queryField,
+          )}QueryResolver(resolver: QueryResolvers["${queryField}"]) {`,
+        );
+        lines.push(`  return resolver;`);
+        lines.push("}");
+      });
+    }
+    if (isObjectType(typeMap.Mutation)) {
+      Object.keys(typeMap.Mutation.getFields()).forEach((mutationField) => {
+        lines.push(
+          `export function define${pascalCase(
+            mutationField,
+          )}MutationResolver(resolver: MutationResolvers["${mutationField}"]) {`,
+        );
+        lines.push(`  return resolver;`);
+        lines.push("}");
+      });
+    }
+
+    lines.push(`export function defineResolvers(resolvers: Resolvers) {`);
+    lines.push(`  return resolvers;`);
+    lines.push(`}`);
+
+    return lines.join("\n");
+  },
+};
